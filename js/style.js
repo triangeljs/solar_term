d0d54eb9aa427028bfdf55f84a21133bc5c3f284
yangfan <yangfan_china@q.com>
2013-04-08 17:12:42 +0800

    项目进行
    优化过场动画

--- js/style.js
+++ js/style.js
@@ -1,12 +1,16 @@
 $(function(){
+	var clientW = document.body.clientWidth;
+	var clientH = document.body.clientHeight;
+	var jq = ['立春','雨水','惊蛰','春分','清明','谷雨','立夏','小满','芒种','夏至','小暑','大暑','立秋','处暑','白露','秋分','寒露','霜降','立冬','小雪','大雪','冬至','小寒','大寒'];
+	var cur = 0;
 	
 	var step1 = function() {
 		// init
-		var synopsisTemplate = _.template($("#synopsisTemplate").html());
-		$('#wrapper').html(synopsisTemplate);
+		var Template = _.template($("#synopsisTemplate").html());
+		$('#wrapper').html(Template);
 		
 		// animate
-		$('#synopsis').css({ '-webkit-transform': 'translate(0px,-40px)', 'opacity': '1' });
+		$('#synopsis h1, #synopsis div').css({ '-webkit-transform': 'translate(0px,-40px)', 'opacity': '1' });
 		$('#synopsis p').css({ 'opacity': '1' });
 		$('#synopsis p').on('mouseup touchend', synopsisOut);
 		
@@ -21,37 +25,115 @@ $(function(){
 	
 	var step2 = function() {
 		// init
-		var infoTemplate = _.template($("#infoTemplate").html());
-		$('#wrapper').html(infoTemplate);
+		var Template = _.template($("#info1Template").html());
+		$('#wrapper').html(Template);
 		
 		// animate
-		$('#info').css({ '-webkit-transform': 'scale(1,1)' });
-		$('#info .xs1').css({ '-webkit-transform': 'translate(0px,-40px)', 'opacity': '0' });
-		$('#info .xs1 h1,#info .xs1 h2,#info .xs1 h3').css({ '-webkit-transform': 'translate(0px,-40px)', 'opacity': '1' });
-		$('#info .xs2').css({ '-webkit-transform': 'translate(0px,-200px)', 'opacity': '1' });
-		$('#info .xs2 h1,#info .xs2 h2,#info .xs2 h3,#info .xs2 h4').css({ '-webkit-transform': 'translate(0px,-40px)', 'opacity': '1' });
-		
-		$('#info .xs2 h4').on('webkitTransitionEnd', function(){
-			$('#info .xs2').css({ '-webkit-transition-delay': '0s', 'opacity': '0' });
-			step3();
-		});
+		$('#info1').css({ '-webkit-transform': 'scale(1,1)' });
+		$('#info1 h1, #info1 h2, #info1 h3').css({ '-webkit-transform': 'translate(0px,-40px)', 'opacity': '1' });
+		window.setTimeout(function(){
+			$('#info1 h1, #info1 h2, #info1 h3').css({ '-webkit-transform': 'translate(0px,-60px)', 'opacity': '0' });
+			$('#info1 h3').on('webkitTransitionEnd', function(){
+				step3();
+			});
+		},3000)
 	};
 	
-	var step3 = function() {		
-		$('#info').css({ '-webkit-transition-delay': '0.5s', '-webkit-transform': 'scale(0.4,0.4)' });
-		$('#info').remove();
-		/*$('#info').on('webkitTransitionEnd', function(){
-			step2();
-		});*/
+	var step3 = function() {
+		// init
+		var Template = _.template($("#info2Template").html());
+		$('#wrapper').html(Template);
 		
-		var discTemplate =  _.template($("#discTemplate").html());
-		$('#wrapper').html(discTemplate);
-		$('#disc').css({ '-webkit-transform': 'scale(0.4,0.4) rotateY(180deg)' });
+		// animate
+		$('#info2 h1, #info2 h2, #info2 h3, #info2 h4').css({ '-webkit-transform': 'translate(0px,-40px)', 'opacity': '1' });
+		window.setTimeout(function(){
+			$('#info2 h1, #info2 h2, #info2 h3, #info2 h4').css({ '-webkit-transform': 'translate(0px,-60px)', 'opacity': '0' });
+			$('#info2 h4').on('webkitTransitionEnd', function(){
+				$('#info2 h4').off('webkitTransitionEnd');
+				step4();
+			});
+		},3000)
+	}
+	
+	var step4 = function() {
+		// init
+		var Template = _.template($("#discTemplate").html());
+		$('#wrapper').remove();
+		$('body').prepend(Template);
+		//$('body').append('<div id="coverBox"></div>');
+		var temp = [];
+			for(var i=0,len=jq.length; i<len; i++){
+				temp.push('<span style="-webkit-transform: rotate(' + i * 15 + 'deg);">'+ jq[i] +'</span>'); 
+			}
+		// animate
+		window.setTimeout(function(){
+			$('#disc').css({ '-webkit-transform': 'scale(0.2,0.2)' });
+		},10)
+		window.setTimeout(function(){
+			$('#disc').prepend(temp.join(''));
+			$('#beautifyDisc').css({ '-webkit-transform': 'scale(1,1)', 'opacity': '1' })
+			//$('#disc').css({ '-webkit-transform': 'scale(1,1) rotate(90deg)', 'background-image': 'url(/solar_term/images/b_5.gif)' })
+			$('#disc').css({ '-webkit-transform': 'scale(1,1)', 'background-image': 'url(/solar_term/images/b_5.gif)' })
+			$('#triangle-up').css({ '-webkit-transform': 'translate(0px,-20px)', 'opacity': '1' })
+			
+			$('#triangle-up').on('webkitTransitionEnd', function(){
+				step5();
+			});
+		},700)
+		
+		var step5 = function() {
+			$('#discBox').css({'-webkit-transform': 'translate(0px, ' + (clientH / 2) + 'px)'});
+			$('#discBox').on('webkitTransitionEnd',function(e){
+				$('#discBox').off('webkitTransitionEnd');
+				$('#discBox').css({
+					'-webkit-transition-duration': '0.5s',
+					'-webkit-transform': 'translate(0px, ' + (clientH / 2) + 'px) scale(0.3,0.3)'
+				})
+				$('#beautifyDisc').on('mouseup touchend', discOut);
+				$('#disc span').on('mouseup touchend', discOpt);
+				$('#coverBox').on('mouseup touchend', coverBoxOut);
+				step6();
+			})
+		}
+		
+		var step6 = function() {
+			var Template = _.template($("#itemTemplate").html());
+			var w = (clientW - 680) / 2;
+			var h = (clientH - 768) / 2
+			$('#itemBox').html(Template).show().css({ 'left': w, 'top': h });
+			$('body').css({ 'background-image': 'none' })
+		}
 	}
 	
 	var run = function() {
 		step1();
 	};
 	run();
-
+	
+	function discOut(e) {
+		$('#coverBox').show();
+		$('#discBox').off('webkitTransitionEnd');
+		$('#discBox').css({ '-webkit-transform': 'translate(0px, ' + (clientH / 2) + 'px) scale(1,1)' });
+	}
+	
+	function discOpt(e) {
+		var nxt = $(this).index();
+		if (nxt < cur && ((nxt + 12) % 24) > cur) {
+			// 顺时针
+			deg = nxt < cur ? cur - nxt : cur + 24 - nxt;
+			$('#disc').css({ '-webkit-transform': 'scale(1,1) rotate('+ deg * 15 +'deg)' })
+			cur = nxt;
+		} else {
+			// 逆时针
+			deg = nxt > cur ? nxt - cur : nxt + 24 - cur;
+			$('#disc').css({ '-webkit-transform': 'scale(1,1) rotate('+ -deg * 15 +'deg)' })
+			cur = nxt * 15;
+		}
+		console.log('nxt:' + nxt + '/deg:' + deg + '/cur:' + cur);
+		
+	}
+	function coverBoxOut(e) {
+		$('#coverBox').hide();
+		$('#discBox').css({ '-webkit-transform': 'translate(0px, ' + (clientH / 2) + 'px) scale(0.3,0.3)' });
+	}
 });
